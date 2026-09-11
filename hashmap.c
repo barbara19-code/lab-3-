bfa89849afcdeb90f3e71a8e950db8a32caa2b4c
Your Name <you@example.com>
2026-09-11 01:08:53 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -53,7 +53,7 @@ HashMap * createMap(long capacity) {
     map -> size = 0;
     map -> current = -1;
     
-    return NULL;
+    return map;
 }
 
 // 2. Implemente la función void insertMap(HashMap * map, char * key, void * value). 
